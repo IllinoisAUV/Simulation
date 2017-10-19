1575522437f28b3041fca4aaded4a3e943c70547
shubhankar12 <shubhankar0109@gmail.com>
2017-10-19 13:57:05 -0500

    Code to generate trajectories. Needs better formamtting

---
+++ trajectory_generator_custom_controls/scripts/trajectory3d_generator.py
@@ -0,0 +1,71 @@
+from pylab import *
+import rospy
+from numpy import linalg
+# from ruamel.yaml import YAML
+import yaml
+
+def ROVsimulation(procVar,measVar,N,dt):
+     #DO NOT CHANGE
+     # Function Given for creating simulated data
+     acc = zeros((3,N))
+     vel = zeros((3,N))
+     control_acc = zeros((3,N))
+     pos = zeros((3,N))
+     meas_pos = zeros((3,N))
+     z = zeros((3,N))
+     for i in arange(1,N):
+             z = normal([0, 0,0], 1.0)- 0.05*control_acc[:,i-1]
+             control_acc[:,i] = control_acc[:,i-1] + z - 0.01*vel[:,i-1]
+             vel[:,i] = vel[:,i-1] + control_acc[:,i-1]*dt
+             pos[:,i] = pos[:,i-1] + vel[:,i-1]*dt + control_acc[:,i-1]*0.5*dt**2
+
+     acc = normal(control_acc, sqrt(procVar))
+     pos = zeros((3,N))
+     vel = zeros((3,N))
+
+     for i in arange(1,N):
+
+             vel[:,i] = vel[:,i-1] + acc[:,i-1]*dt
+             pos[:,i] = pos[:,i-1] + vel[:,i-1]*dt + acc[:,i-1]*0.5*dt**2
+
+
+     meas_pos = normal(pos, sqrt(measVar))
+     real_pos = pos
+
+     return control_acc, meas_pos, real_pos
+
+# N = int(1e4)                            # number of simulation sample point
+N = 100000
+dt = 1.0/100.0          # time interval for each sample point
+procVar = 0.001         # Process(Prediction) Noise
+measVar = 3                     # Measurement Noise
+
+[control_acc, meas_pos, real_pos] = ROVsimulation(procVar,measVar,N,dt)
+
+pos= []
+
+for i in range(N)[0::100]:
+    pos.append(real_pos[:,i])
+    # print str(int(meas_pos[:,i][0])) + ' ' + str(int(meas_pos[:,i][1])) + ' ' + str(int(meas_pos[:,i][2]))
+
+code = yaml.load(u"""
+{'max_forward_speed': 0.4, 'use_fixed_heading': False, 'heading': 0, 'point': [0, 2, -22]}
+""")
+
+pos = pos[:100]
+
+print len(pos)
+
+# stream1 = file('example_waypoints.yaml', 'r')
+# code = yaml.load(stream1)
+stream = file('3D_traj_100.yaml', 'w')
+print code
+for i in pos:
+    if (  int(i[2]) > 0):
+        i[2] = -1 * int(i[2])
+    code['point'] = (int(i[0]), int(i[1]), int(i[2]))
+    # code['max_forward_speed'] = 0.2
+    # code['heading'] = 0
+    # code['use_fixed_heading'] = False
+    stream.write("-\n")
+    yaml.dump(code, stream, indent= 8 )
